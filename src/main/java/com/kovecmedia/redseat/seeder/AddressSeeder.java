9550d9f1bf33883b77c7f879e925c76f30d5baf5
Kevonia <Tomlinson>
2020-08-20 22:01:40 -0500

    no seeding

--- src/main/java/com/kovecmedia/redseat/seeder/AddressSeeder.java
+++ src/main/java/com/kovecmedia/redseat/seeder/AddressSeeder.java
@@ -35,7 +35,7 @@ public class AddressSeeder {
 			NumberFormat formatter = new DecimalFormat("#0.00000");
 			
 			long start = System.currentTimeMillis();
-			for (int i = 1; i <= 200; i++) {
+			for (int i = 1; i <= 10; i++) {
 				Address address = new Address();
 			     address.setAddressline1(faker.address().streetAddress());
 			     address.setAddressline2(faker.address().secondaryAddress());
